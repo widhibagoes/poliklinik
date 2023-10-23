cb7e4c9e75caf4d7cc1d420cafddf3dce1954d39
widhibagoes <widhi.bagoes.wb@gmail.com>
2023-10-23 08:47:08 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      Sistem Informasi Poliklinik
+    </a>
+    <button class="navbar-toggler"
+    type="button" data-bs-toggle="collapse"
+    data-bs-target="#navbarNavDropdown"
+    aria-controls="navbarNavDropdown" aria-expanded="false"
+    aria-label="Toggle navigation">
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="index.php">
+            Home
+          </a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button"
+          data-bs-toggle="dropdown" aria-expanded="false">
+            Data Master
+          </a>
+          <ul class="dropdown-menu">
+            <li>
+              <a class="dropdown-item" href="index.php?page=dokter">
+                Dokter
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="index.php?page=pasien">
+                Pasien
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" 
+          href="index.php?page=periksa">
+            Periksa
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<main role="main" class="container">
+    <?php
+    if (isset($_GET['page'])) {
+    ?>
+        <h2><?php echo ucwords($_GET['page']) ?></h2>
+    <?php
+        include($_GET['page'] . ".php");
+    } else {
+        echo "Selamat Datang di Sistem Informasi Poliklinik";
+    }
+    ?>
+</main>
